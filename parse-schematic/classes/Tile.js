d449f3fdfdc8dc297c40f451e417e0744f3ad2e2
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2022-05-22 21:06:53 +0530

    There's the issue

--- parse-schematic/classes/Tile.js
+++ parse-schematic/classes/Tile.js
@@ -1,6 +1,7 @@
 import { SmartBuffer } from "../ported/SmartBuffer.js";
 import { Point2 } from "../ported/Point2.js";
 import * as zlib from "zlib";
+import { toHexCodes } from "../funcs.js";
 export class Tile {
     constructor(name, position, config, rotation) {
         this.name = name;
@@ -16,9 +17,11 @@ export class Tile {
         return Tile.logicBlocks.includes(this.name);
     }
     static decompressLogicCode(rawData) {
+        console.log("Decompressing code: ", toHexCodes(Buffer.from(rawData)).join(" "));
         let data = new SmartBuffer({
             buff: zlib.inflateSync(Uint8Array.from(rawData))
         });
+        console.log("Decompressed code: ", toHexCodes(data.toBuffer()).join(" "));
         let version = data.readInt8();
         if (version != 1)
             throw new Error(`Unsupported logic code of version ${version}`);
@@ -29,12 +32,15 @@ export class Tile {
     static compressLogicCode(code) {
         let output = new SmartBuffer();
         output.writeInt8(Tile.logicVersion);
-        let outputCode = code.join("\n");
+        let outputCode = Buffer.from(code.join("\n"));
         output.writeInt32BE(outputCode.length);
-        output.writeBuffer(Buffer.from(outputCode));
+        output.writeBuffer(outputCode);
         //TODO links
         output.writeInt32BE(0);
-        return Array.from(zlib.deflateSync(output.toBuffer()));
+        console.log("Precompressed code: ", toHexCodes(output.toBuffer()).join(" "));
+        let compressedData = zlib.deflateSync(output.toBuffer());
+        console.log("Compressed code: ", toHexCodes(compressedData).join(" "));
+        return Array.from(compressedData);
     }
 }
 Tile.logicBlocks = ["micro-processor", "logic-processor", "hyper-processor"];
--- parse-schematic/classes/Tile.ts
+++ parse-schematic/classes/Tile.ts
@@ -2,6 +2,7 @@ import { SmartBuffer } from "../ported/SmartBuffer.js";
 import { Point2 } from "../ported/Point2.js";
 import { BlockConfig } from "./BlockConfig.js";
 import * as zlib from "zlib";
+import { toHexCodes } from "../funcs.js";
 
 export class Tile {
 	x: number;
@@ -19,9 +20,11 @@ export class Tile {
 		return Tile.logicBlocks.includes(this.name);
 	}
 	static decompressLogicCode(rawData:number[]):string[] {
+		console.log("Decompressing code: ", toHexCodes(Buffer.from(rawData)).join(" "));
 		let data = new SmartBuffer({
 			buff: zlib.inflateSync(Uint8Array.from(rawData))
 		});
+		console.log("Decompressed code: ", toHexCodes(data.toBuffer()).join(" "));
 		let version = data.readInt8();
 		if(version != 1) throw new Error(`Unsupported logic code of version ${version}`);
 		let length = data.readInt32BE();
@@ -32,14 +35,17 @@ export class Tile {
 		let output = new SmartBuffer();
 		output.writeInt8(Tile.logicVersion);
 
-		let outputCode = code.join("\n")
+		let outputCode = Buffer.from(code.join("\n"));
 		output.writeInt32BE(outputCode.length);
-		output.writeBuffer(Buffer.from(outputCode));
+		output.writeBuffer(outputCode);
 
 		//TODO links
 		output.writeInt32BE(0);
 
-		return Array.from(zlib.deflateSync(output.toBuffer()));
+		console.log("Precompressed code: ", toHexCodes(output.toBuffer()).join(" "));
+		let compressedData = zlib.deflateSync(output.toBuffer());
+		console.log("Compressed code: ", toHexCodes(compressedData).join(" "));
+		return Array.from(compressedData);
 	}
 	
 
