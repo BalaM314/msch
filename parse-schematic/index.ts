c525bb84d67a03b3e07dd22897ac6d3e81c8bcb6
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2022-05-24 14:36:07 +0530

    cleanup

--- parse-schematic/index.ts
+++ parse-schematic/index.ts
@@ -5,7 +5,6 @@ import * as fs from "fs";
 import * as path from "path";
 import { parseArgs } from "./funcs.js";
 import { Schematic } from "./classes/Schematic.js";
-import { Tile } from "./classes/Tile.js";
 
 
 
