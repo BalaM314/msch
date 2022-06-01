d9b52d45f45809aaf1e89078eab2c4613025fad7
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2022-06-01 17:51:35 +0530

    Convert to good library

---
+++ msch/index.js
@@ -0,0 +1,8 @@
+import { SmartBuffer } from "smart-buffer";
+import { BlockConfig } from "./classes/BlockConfig.js";
+import { Schematic } from "./classes/Schematic.js";
+import { Tile } from "./classes/Tile.js";
+import { Point2 } from "./ported/Point2.js";
+import { TypeIO } from "./ported/TypeIO.js";
+import { BlockConfigType } from "./types.js";
+export { Schematic, Tile, BlockConfig, Point2, TypeIO, SmartBuffer, BlockConfigType };
---
+++ msch/index.ts
@@ -0,0 +1,14 @@
+import { SmartBuffer } from "smart-buffer";
+import { BlockConfig } from "./classes/BlockConfig.js";
+import { Schematic } from "./classes/Schematic.js";
+import { Tile } from "./classes/Tile.js";
+import { Point2 } from "./ported/Point2.js";
+import { TypeIO } from "./ported/TypeIO.js";
+import { BlockConfigType, BlockConfigValue } from "./types.js";
+
+
+export {
+	Schematic, Tile, BlockConfig,
+	Point2, TypeIO, SmartBuffer,
+	BlockConfigType, BlockConfigValue
+};
