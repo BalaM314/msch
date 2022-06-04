85c8aabaafc9cf7c91a2383abe36ce5e1cbb70bf
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2022-06-04 07:45:38 +0530

    Refactor Tile.(de)?compressLogicConfig()

--- classes/Tile.ts
+++ classes/Tile.ts
@@ -47,47 +47,66 @@ export class Tile {
 	isProcessor(){
 		return Tile.logicBlocks.includes(this.name);
 	}
-	/**Decompresses the config into links and code. */
-	decompressLogicConfig(){
-		if(!this.isProcessor()) throw new Error("not a processor");
+	/**Decompresses a processor config into links and code. */
+	static decompressLogicConfig(config:BlockConfig){
 		let data = new SmartBuffer({
-			buff: zlib.inflateSync(Uint8Array.from(this.config.value as number[]))
+			buff: zlib.inflateSync(Uint8Array.from(config.value as number[]))
 		});
 		let version = data.readInt8();
 		if(version != 1) throw new Error(`Unsupported logic code of version ${version}`);
 		let length = data.readInt32BE();
-		this.code = data.readBuffer(length).toString().split(/\r?\n/g);
+		let code = data.readBuffer(length).toString().split(/\r?\n/g);
 		
 		let numLinks = data.readInt32BE();
-		this.links ??= [];
+		let links = [];
 		for(let i = 0; i < numLinks; i ++){
-			this.links.push({
+			links.push({
 				name: data.readUTF8(),
 				x: data.readInt16BE(),
 				y: data.readInt16BE()
 			});
 		}
+		return { code, links };
+	}
+	decompressLogicConfig(){
+		if(!this.isProcessor()) throw new Error("not a processor");
+		let { code, links } = Tile.decompressLogicConfig(this.config);
+		this.code = code;
+		this.links = links;
 	}
 	/**Compresses links and code for serialization. */
-	compressLogicConfig(){
-		if(!this.isProcessor()) throw new Error("not a processor");
-		if(!this.links || !this.code) throw new Error("Data not decompressed");
+	static compressLogicConfig({links, code}: {
+		links: {
+			name: string;
+			x: number;
+			y: number;
+		}[];
+		code: string[];
+	}){
 		let output = new SmartBuffer();
 		output.writeInt8(Tile.logicVersion);
 
-		let outputCode = Buffer.from(this.code.join("\n"));
+		let outputCode = Buffer.from(code.join("\n"));
 		output.writeInt32BE(outputCode.length);
 		output.writeBuffer(outputCode);
 
-		output.writeInt32BE(this.links.length);
-		for(let link of this.links){
+		output.writeInt32BE(links.length);
+		for(let link of links){
 			output.writeUTF8(link.name);
 			output.writeInt16BE(link.x);
 			output.writeInt16BE(link.y);
 		}
 
 		let compressedData = zlib.deflateSync(output.toBuffer());
-		this.config.value = Array.from(compressedData);
+		return Array.from(compressedData);
+	}
+	compressLogicConfig(){
+		if(!this.isProcessor()) throw new Error("not a processor");
+		if(!this.links || !this.code) throw new Error("data was never decompressed");
+		return Tile.compressLogicConfig({
+			links: this.links,
+			code: this.code
+		});
 	}
 	/**Used for displaying config. */
 	formatConfig() {
